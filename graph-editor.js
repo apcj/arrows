4821183a904e5504a2f071e8c89bab4b20d37449
Alistair Jones <alistair.jones@gmail.com>
2012-09-02 11:56:53 +0100

    Store markup in browser localStorage.

--- graph-editor.js
+++ graph-editor.js
@@ -1,6 +1,13 @@
-(function() {
-    var graphModel = gd.model();
-    graphModel.createNode().x(50).y(140);
+(function()
+{
+    var graphModel;
+    if (localStorage.getItem("graph-diagram-markup")) {
+        graphModel = parseMarkup( localStorage.getItem( "graph-diagram-markup" ) );
+        console.log(graphModel.nodeList());
+    } else {
+        graphModel = gd.model();
+        graphModel.createNode().x( 50 ).y( 140 );
+    }
 
     var svg = d3.select("#canvas")
         .append("svg:svg")
@@ -17,23 +24,30 @@
             .on("dblclick", editRelationship);
     }
 
-    draw();
+    function save( markup )
+    {
+        localStorage.setItem( "graph-diagram-markup", markup );
+    }
 
     var newNode = null;
     var newRelationship = null;
 
-    function findClosestOverlappingNode(node) {
+    function findClosestOverlappingNode( node )
+    {
 
         var closestNode = null;
         var closestDistance = Number.MAX_VALUE;
 
         var allNodes = graphModel.nodeList();
 
-        for (var i = 0; i < allNodes.length; i++) {
+        for ( var i = 0; i < allNodes.length; i++ )
+        {
             var candidateNode = allNodes[i];
-            if (candidateNode !== node) {
-                var candidateDistance = node.distanceTo(candidateNode) * graphModel.internalScale();
-                if (candidateDistance < 50 && candidateDistance < closestDistance) {
+            if ( candidateNode !== node )
+            {
+                var candidateDistance = node.distanceTo( candidateNode ) * graphModel.internalScale();
+                if ( candidateDistance < 50 && candidateDistance < closestDistance )
+                {
                     closestNode = candidateNode;
                     closestDistance = candidateDistance;
                 }
@@ -42,129 +56,166 @@
         return closestNode;
     }
 
-    function drag(){
+    function drag()
+    {
         var shiftKey = window.event.shiftKey;
-        var dragTarget = d3.select(this);
+        var dragTarget = d3.select( this );
         var node = dragTarget[0][0].__data__;
-        if (!newNode && shiftKey) {
-            newNode = graphModel.createNode().x(node.x()).y(node.y());
-            newRelationship = graphModel.createRelationship(node, newNode);
+        if ( !newNode && shiftKey )
+        {
+            newNode = graphModel.createNode().x( node.x() ).y( node.y() );
+            newRelationship = graphModel.createRelationship( node, newNode );
         }
-        if (newNode) {
-            var connectionNode = findClosestOverlappingNode(newNode);
-            if (connectionNode) {
+        if ( newNode )
+        {
+            var connectionNode = findClosestOverlappingNode( newNode );
+            if ( connectionNode )
+            {
                 newRelationship.end = connectionNode
-            } else {
+            } else
+            {
                 newRelationship.end = newNode;
             }
             node = newNode;
         }
-        node.x(node.x() + d3.event.dx);
-        node.y(node.y() + d3.event.dy);
+        node.x( node.x() + d3.event.dx );
+        node.y( node.y() + d3.event.dy );
         draw();
     }
 
-    function dragEnd() {
-        if (newNode && newRelationship && newRelationship.end !== newNode) {
-            graphModel.deleteNode(newNode);
+    function dragEnd()
+    {
+        if ( newNode && newRelationship && newRelationship.end !== newNode )
+        {
+            graphModel.deleteNode( newNode );
         }
         newNode = null;
+        save( formatMarkup() );
         draw();
     }
 
-    function editNode() {
+    function editNode()
+    {
         var node = this.__data__;
-        var field = svg.append("svg:foreignObject")
-            .attr("x", node.x() - 50)
-            .attr("y", node.y() - 50)
-            .attr("height", 100)
-            .attr("width", 120)
-            .append("xhtml:body")
-            .append("input")
-            .attr("class", "editor-field")
-            .style("width", 100 + "px");
+        var field = svg.append( "svg:foreignObject" )
+            .attr( "x", node.x() - 50 )
+            .attr( "y", node.y() - 50 )
+            .attr( "height", 100 )
+            .attr( "width", 120 )
+            .append( "xhtml:body" )
+            .append( "input" )
+            .attr( "class", "editor-field" )
+            .style( "width", 100 + "px" );
 
         field.node().value = node.label() || "";
         field.node().select();
-        field.on("keypress", function() {
+        field.on( "keypress", function ()
+        {
             var e = d3.event;
-            if (e.which == 10 || e.which == 13) {
-                node.label(field.node().value);
+            if ( e.which == 10 || e.which == 13 )
+            {
+                node.label( field.node().value );
                 field.remove();
+                save( formatMarkup() );
                 draw();
             }
-        })
+        } )
     }
 
-    function editRelationship() {
+    function editRelationship()
+    {
         var relationship = this.__data__;
-        var midwayPoint = relationship.start.midwayTo(relationship.end);
-        var field = svg.append("svg:foreignObject")
-            .attr("x", midwayPoint.x - 50)
-            .attr("y", midwayPoint.y - 50)
-            .attr("height", 100)
-            .attr("width", 120)
-            .append("xhtml:body")
-            .append("input")
-            .attr("class", "editor-field")
-            .style("width", 100 + "px");
+        var midwayPoint = relationship.start.midwayTo( relationship.end );
+        var field = svg.append( "svg:foreignObject" )
+            .attr( "x", midwayPoint.x - 50 )
+            .attr( "y", midwayPoint.y - 50 )
+            .attr( "height", 100 )
+            .attr( "width", 120 )
+            .append( "xhtml:body" )
+            .append( "input" )
+            .attr( "class", "editor-field" )
+            .style( "width", 100 + "px" );
 
         field.node().value = relationship.label() || "";
         field.node().select();
-        field.on("keypress", function() {
+        field.on( "keypress", function ()
+        {
             var e = d3.event;
-            if (e.which == 10 || e.which == 13) {
-                relationship.label(field.node().value);
+            if ( e.which == 10 || e.which == 13 )
+            {
+                relationship.label( field.node().value );
                 field.remove();
+                save( formatMarkup() );
                 draw();
             }
-        })
+        } )
     }
 
-    var exportMarkup = function() {
-        var container = d3.select("body" ).append("div");
-        gd.markup.format(graphModel, container);
+    function formatMarkup()
+    {
+        var container = d3.select( "body" ).append( "div" );
+        gd.markup.format( graphModel, container );
         var markup = container.node().innerHTML;
         markup = markup
-            .replace(/<li/g, "\n  <li")
-            .replace(/<span/g, "\n    <span")
-            .replace(/<\/span><\/li/g, "</span>\n  </li")
-            .replace(/<\/ul/, "\n</ul");
-
+            .replace( /<li/g, "\n  <li" )
+            .replace( /<span/g, "\n    <span" )
+            .replace( /<\/span><\/li/g, "</span>\n  </li" )
+            .replace( /<\/ul/, "\n</ul" );
         container.remove();
-        d3.selectAll(".modal-appear")
-            .style("display", "block");
+        return markup;
+    }
 
-        d3.select("textarea.code")
-            .attr("rows", markup.split("\n" ).length)
+    var exportMarkup = function ()
+    {
+        d3.selectAll( ".modal-appear" )
+            .style( "display", "block" );
+
+        var markup = formatMarkup();
+        d3.select( "textarea.code" )
+            .attr( "rows", markup.split( "\n" ).length )
             .node().value = markup;
     };
 
-    var saveMarkup = function() {
-        var markup = d3.select("textarea.code").node().value;
-        var container = d3.select("body" ).append("div");
+    function parseMarkup( markup )
+    {
+        var container = d3.select( "body" ).append( "div" );
         container.node().innerHTML = markup;
-        graphModel = gd.markup.parse(container);
+        var model = gd.markup.parse( container );
         container.remove();
+        return model;
+    }
+
+    var useMarkupFromMarkupEditor = function ()
+    {
+        var markup = d3.select( "textarea.code" ).node().value;
+        graphModel = parseMarkup( markup );
+        save( markup );
         draw();
 
-        d3.selectAll(".modal-appear")
-            .style("display", "none");
+        d3.selectAll( ".modal-appear" )
+            .style( "display", "none" );
     };
 
-    var exportSvg = function() {
-        d3.xhr("graph-diagram.css", function(d) {
+    var exportSvg = function ()
+    {
+        d3.xhr( "graph-diagram.css", function ( d )
+        {
             var css = d.responseText;
-            var rawSvg = document.getElementById("canvas").innerHTML;
+            var rawSvg = document.getElementById( "canvas" ).innerHTML;
             var styleTag = "<style type=\"text/css\"><![CDATA[" + css + "]]></style>";
             var svgStartTag = "<svg xmlns=\"http://www.w3.org/2000/svg\">";
-            var modifiedSvg = rawSvg.replace(/<svg [^>]*>/, svgStartTag + styleTag);
-            window.open("data:image/svg+xml;base64," + btoa(modifiedSvg));
-        });
+            var modifiedSvg = rawSvg.replace( /<svg [^>]*>/, svgStartTag + styleTag );
+            window.open( "data:image/svg+xml;base64," + btoa( modifiedSvg ) );
+        } );
     };
 
-    d3.select("#exportMarkupButton").on("click", exportMarkup);
-    d3.select("#exportSvgButton").on("click", exportSvg);
-    d3.select("#modal-container").on("click", saveMarkup);
-    d3.select("#modal-dialog").on("click", function() { d3.event.stopPropagation(); });
+    d3.select( "#exportMarkupButton" ).on( "click", exportMarkup );
+    d3.select( "#exportSvgButton" ).on( "click", exportSvg );
+    d3.select( "#modal-container" ).on( "click", useMarkupFromMarkupEditor );
+    d3.select( "#modal-dialog" ).on( "click", function ()
+    {
+        d3.event.stopPropagation();
+    } );
+
+    draw();
 })();
